946450cd7354f2054d3f2453408d8eae11feb06d
667-N <jaya.bhatia@stud.onu.edu.ua>
2025-03-20 07:17:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
- const app = express();
- const PORT = 3000;
+const path = require('path');
+const app = express();
+const PORT = 3000;
 
-    // Serve static files (optional, if you have CSS, images, etc.)
-    app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-    // API endpoint
-    app.get('/api', (req, res) => {
-                                     res.send('Hello API World from Server!');
-                                  });
+app.get('/api', (req, res) => {
+  res.send('Hello API World from Server!');
+});
 
-    // Serve client page
-    app.get('/', (req, res) => {
-                                res.sendFile(__dirname + '/index.html');
-                               });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-     app.listen(PORT, '0.0.0.0', () => {
-                                          console.log(Server running at ${PORT});
-                                        });
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
